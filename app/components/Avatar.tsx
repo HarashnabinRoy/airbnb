c3dc9d17b9436449719e50047075ec6298366f8c
Exion <harashnabin2002@gmail.com>
2024-01-03 18:50:30 +0530

    Login OAuth Complete

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import Image from 'next/image'
 
-const Avatar = () => {
+interface AvatarProps {
+  src: string | null | undefined
+}
+
+const Avatar: React.FC<AvatarProps> = ({
+  src
+}) => {
   return (
-    <Image className='rounded-full' height={30} width={30} alt="Avatar" src="/Images/placeholder.jpg" />
+    <Image className='rounded-full' height={30} width={30} alt="Avatar" src= { src || "/Images/placeholder.jpg" } />
   )
 }
 
