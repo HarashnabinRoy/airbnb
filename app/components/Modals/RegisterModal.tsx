b47fb576ad51d802bfcc7d2aa14a97b000eb95b6
Exion <harashnabin2002@gmail.com>
2024-01-11 21:00:43 +0530

    UserMenu Items added

--- app/components/Modals/RegisterModal.tsx
+++ app/components/Modals/RegisterModal.tsx
@@ -40,6 +40,8 @@ const RegisterModal = () => {
         axios.post('/api/register', data)
             .then(() => {
                 registerModal.onClose();
+                loginModal.onOpen();
+                toast.success('Signed In')
             })
             .catch((error)=> {
                 toast.error("Something went wrong!!");
