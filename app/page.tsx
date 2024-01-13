d1bd47f561d2d06bbb0f0c1def9b8611df8d217f
Exion <harashnabin2002@gmail.com>
2024-01-13 12:27:03 +0530

    Properties page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
-import getListings from "./actions/getListings";
+import getListings, { IListingParams } from "./actions/getListings";
 import ClientOnly from "./components/ClientOnly";
 import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
 import ListingCard from "./components/Listings/ListingCard";
 import getCurrentUser from "@/app/actions/getCurrentUser";
 
-export default async function Home() {
-  const isEmpty = true;
-  const listings = await getListings();
+interface HomeProps {
+  searchParams: IListingParams;
+}
+
+const Home = async ({searchParams}: HomeProps) => {
+  // const isEmpty = true;
+  const listings = await getListings(searchParams);
   const currentUser = await getCurrentUser();
 
   if(listings.length === 0){
@@ -37,3 +41,5 @@ export default async function Home() {
     </ClientOnly>
   )
 }
+
+export default Home;
